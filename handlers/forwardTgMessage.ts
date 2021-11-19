023a71b443839ecb3be7eb90bf2bc5b28ba860c1
凌莞 <i@gao4.pw>
2021-11-19 20:15:35 +0800

    删除调试输出

--- handlers/forwardTgMessage.ts
+++ handlers/forwardTgMessage.ts
@@ -8,7 +8,6 @@ import {forwardOff, qq, tg} from '../index'
 import handleTgMsgDelete from './handleTgMsgDelete'
 
 export default async (msg: TelegramBot.Message) => {
-    console.log(msg)
     try {
         if (msg.chat.id > 0) {
             if (msg.text && msg.text.startsWith('/start ')) {
