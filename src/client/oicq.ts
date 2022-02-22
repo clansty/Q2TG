bf34bb617178ec6d182bc11d3162b2d98307a7c8
凌莞 <i@gao4.pw>
2022-02-22 14:20:23 +0800

    feat: 可以自己生成设备文件

--- src/client/oicq.ts
+++ src/client/oicq.ts
@@ -1,5 +1,9 @@
 import { Client, createClient, Platform } from 'oicq';
 import * as Buffer from 'buffer';
+import { execSync } from 'child_process';
+import random from '../utils/random';
+import fs from 'fs';
+import fsP from 'fs/promises';
 
 interface CreateOicqParams {
   uin: number;
@@ -14,7 +18,7 @@ interface CreateOicqParams {
 }
 
 export default function createOicq(params: CreateOicqParams) {
-  return new Promise<Client>((resolve, reject) => {
+  return new Promise<Client>(async (resolve, reject) => {
     async function loginDeviceHandler({ phone }: { url: string, phone: string }) {
       client.sendSmsCode();
       const code = await params.onVerifyDevice(phone);
@@ -49,9 +53,33 @@ export default function createOicq(params: CreateOicqParams) {
       resolve(client);
     }
 
+    if (!fs.existsSync(`./data/${params.uin}/device-${params.uin}.json`)) {
+      !fs.existsSync('./data') && await fsP.mkdir('./data');
+      !fs.existsSync(`./data/${params.uin}`) && await fsP.mkdir(`./data/${params.uin}`);
+
+      const device = {
+        product: 'Q2TG',
+        device: 'ANGELKAWAII2',
+        board: 'raincandy',
+        brand: random.pick('GOOGLE', 'XIAOMI', 'HUAWEI', 'SAMSUNG', 'SONY'),
+        model: 'raincandy',
+        wifi_ssid: random.pick('OpenWrt', `Redmi-${random.hex(4)}`,
+          `MiWifi-${random.hex(4)}`, `TP-LINK-${random.hex(6)}`),
+        bootloader: random.pick('U-Boot', 'GRUB', 'gummiboot'),
+        android_id: random.hex(16),
+        proc_version: `${execSync('uname -s').toString().replace('\n', '')} version ${execSync('uname -r').toString().replace('\n', '')}`,
+        mac_address: `8c:85:90:${random.hex(2)}:${random.hex(2)}:${random.hex(2)}`.toUpperCase(),
+        ip_address: `192.168.${random.int(1, 200)}.${random.int(10, 250)}`,
+        incremental: random.int(0, 4294967295),
+        imei: random.imei(),
+      };
+
+      await fsP.writeFile(`./data/${params.uin}/device-${params.uin}.json`, JSON.stringify(device, null, 0), 'utf-8');
+    }
+
     const client = createClient(params.uin, {
       platform: params.platform,
-      data_dir: './data'
+      data_dir: './data',
     })
       .on('system.login.device', loginDeviceHandler)
       .on('system.login.slider', loginSliderHandler)
---
+++ src/utils/random.ts
@@ -0,0 +1,56 @@
+const random = {
+  int(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值
+  },
+  hex(length: number) {
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += random.int(0, 15).toString(16);
+    }
+    return result;
+  },
+  pick<T>(...array: T[]) {
+    const index = random.int(0, array.length - 1);
+    return array[index];
+  },
+  fakeUuid() {
+    return `${random.hex(8)}-${random.hex(4)}-${random.hex(4)}-${random.hex(4)}-${random.hex(12)}`;
+  },
+  imei() {
+    const uin = random.int(1000000, 4294967295);
+    let imei = uin % 2 ? '86' : '35';
+    const buf = Buffer.alloc(4);
+    buf.writeUInt32BE(uin);
+    let a: number | string = buf.readUInt16BE();
+    let b: number | string = Buffer.concat([Buffer.alloc(1), buf.slice(1)]).readUInt32BE();
+    if (a > 9999)
+      a = Math.trunc(a / 10);
+    else if (a < 1000)
+      a = String(uin).substring(0, 4);
+    while (b > 9999999)
+      b = b >>> 1;
+    if (b < 1000000)
+      b = String(uin).substring(0, 4) + String(uin).substring(0, 3);
+    imei += a + '0' + b;
+
+    function calcSP(imei: string) {
+      let sum = 0;
+      for (let i = 0; i < imei.length; ++i) {
+        if (i % 2) {
+          let j = parseInt(imei[i]) * 2;
+          sum += j % 10 + Math.floor(j / 10);
+        }
+        else {
+          sum += parseInt(imei[i]);
+        }
+      }
+      return (100 - sum) % 10;
+    }
+
+    return imei + calcSP(imei);
+  },
+};
+
+export default random;
