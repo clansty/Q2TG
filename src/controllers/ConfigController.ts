15a45eb85f3cbdaff0ad15bd5941061d12bab522
Clansty <i@gao4.pw>
2022-03-08 14:46:56 +0800

    fix: TG 群组删除时解除关联

--- src/controllers/ConfigController.ts
+++ src/controllers/ConfigController.ts
@@ -95,7 +95,8 @@ export default class ConfigController {
   };
 
   private handleChannelParticipant = async (event: Api.UpdateChannelParticipant) => {
-    if (event.prevParticipant instanceof Api.ChannelParticipantSelf && !event.newParticipant) {
+    if ('userId' in event.prevParticipant && event.prevParticipant.userId.eq(this.tgBot.me.id) &&
+      !event.newParticipant) {
       this.log.warn(`群 ${event.channelId.toString()} 删除了`);
       const pair = this.instance.forwardPairs.find(event.channelId);
       if (pair) {
