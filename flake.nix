d9655fd0c8676de2ef10c956b92a1c0f66bcfb8a
Clansty <i@gao4.pw>
2022-09-13 17:32:53 +0800

    perf: use flake direnv

--- flake.nix
+++ flake.nix
@@ -3,14 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
     {
       packages = nixpkgs.lib.mapAttrs
         (system: pkgs: {
           default = import ./default.nix { inherit pkgs; };
         })
         nixpkgs.legacyPackages;
-        nixosModules.default = import ./nixos/module.nix;
-    };
+      nixosModules.default = import ./nixos/module.nix;
+    } // flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
 }
--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ pkgs.mkShell {
     nodejs-18_x
     python3
     ffmpeg
-    (callPackage ./nixos/prismaPatched.nix {})
+    (callPackage ./nixos/prismaPatched.nix { })
     pkg-config
     (vips.override {
       libjxl = pkgs.libjxl.overrideAttrs (attrs: {
@@ -13,11 +13,10 @@ pkgs.mkShell {
       });
     })
   ];
-  shellHook = ''
-    export PRISMA_MIGRATION_ENGINE_BINARY="${pkgs.prisma-engines}/bin/migration-engine"
-    export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
-    export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
-    export PRISMA_INTROSPECTION_ENGINE_BINARY="${pkgs.prisma-engines}/bin/introspection-engine"
-    export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
-  '';
+
+  PRISMA_MIGRATION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/migration-engine";
+  PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+  PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+  PRISMA_INTROSPECTION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/introspection-engine";
+  PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
 }
