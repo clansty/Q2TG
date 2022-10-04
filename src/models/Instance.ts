5565f3e6e0fc763ec6499553fdf5b078d79bab21
Clansty <i@gao4.pw>
2022-10-04 14:59:57 +0800

    fix: 清除扫码登录残留
    fix #52

--- src/models/Instance.ts
+++ src/models/Instance.ts
@@ -116,11 +116,8 @@ export default class Instance {
           },
           onVerifySlider: async (url) => {
             const res = await this.waitForOwnerInput(`收到滑块验证码 <code>${url}</code>\n` +
-              '请使用<a href="https://github.com/mzdluo123/TxCaptchaHelper/releases">此软件</a>验证并输入 Ticket\n' +
-              '或者点击下方的按钮切换到扫码登录', [
-              Button.text('切换到扫码登录', true, true),
-            ]);
-            if (res === '切换到扫码登录') return '';
+              '请使用<a href="https://github.com/mzdluo123/TxCaptchaHelper/releases">此软件</a>验证并输入 Ticket'
+              );
             return res;
           },
         });
