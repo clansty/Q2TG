b4163b13f91a3f9a6e5b7dd65621c66f9e88137a
Clansty <i@gao4.pw>
2022-10-12 15:10:34 +0800

    deps: update flake

--- nixos/clansty-flake.nix
+++ nixos/clansty-flake.nix
@@ -1,7 +1,6 @@
 pkgs: pkgs.fetchFromGitHub {
   owner = "Clansty";
   repo = "flake";
-  rev = "d602b0359456457b8d92d1f84e42e7aec3baa9be";
-  fetchSubmodules = true;
-  sha256 = "na+HvJ7B/rP+qew1b58hTkHzc6BRvOERUHgva7cEY5k=";
+  rev = "713bfc0c7d3048a88e8db001af51a606672851ba";
+  sha256 = "bIUOHs538LCfmsX2gdKb9A0iAnDV62znP05FIomFir8=";
 }
