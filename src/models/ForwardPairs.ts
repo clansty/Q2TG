2864d1db8fed8d81798bd894fe3b77e6b883e0bd
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-10-18 15:06:44 +0800

    🐛 Try to fix

--- src/models/ForwardPairs.ts
+++ src/models/ForwardPairs.ts
@@ -26,7 +26,7 @@ export default class ForwardPairs {
         const qq = oicq.getChat(Number(i.qqRoomId));
         const tg = await tgBot.getChat(Number(i.tgChatId));
         if (qq && tg) {
-          this.pairs.push(new Pair(qq, tg, i.id, i.joinNotice, i.poke, i.enable));
+          this.pairs.push(new Pair(qq, tg, i.id, i.joinNotice, i.poke, i.enable, i.disableQ2TG, i.disableTG2Q));
         }
       }
       catch (e) {
@@ -49,7 +49,7 @@ export default class ForwardPairs {
         instanceId: this.instanceId,
       },
     });
-    this.pairs.push(new Pair(qq, tg, dbEntry.id, true, true, true));
+    this.pairs.push(new Pair(qq, tg, dbEntry.id, true, true, true, false, false));
     return dbEntry;
   }
 
