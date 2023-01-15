3b1943961a10f4c716d7a25e5e712bd69a334ca6
Clansty <i@gao4.pw>
2023-01-15 21:20:34 +0800

    fix: nixosModules missing

--- flake.nix
+++ flake.nix
@@ -7,25 +7,26 @@
     nix2container.url = "github:nlewo/nix2container";
   };
   outputs = { self, nixpkgs, flake-utils, nix2container }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          flakePkgs = self.packages.${system};
-          nix2containerPkgs = nix2container.packages.${system};
-        in
-        {
-          packages = {
-            default = import ./default.nix { inherit pkgs; };
-            tgs-to-gif = pkgs.callPackage "${import ./nixos/clansty-flake.nix}/packages/tgs-to-gif" { };
-            prisma-patched = pkgs.callPackage ./nixos/prismaPatched.nix { };
-            fontsSf = pkgs.callPackage ./nixos/fontsSf.nix { };
-            docker = import ./docker.nix {
-              inherit pkgs flakePkgs;
-              nix2container = nix2containerPkgs.nix2container;
-            };
+    {
+      nixosModules.default = import ./nixos/module.nix { inherit self; };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        flakePkgs = self.packages.${system};
+        nix2containerPkgs = nix2container.packages.${system};
+      in
+      {
+        packages = {
+          default = import ./default.nix { inherit pkgs; };
+          tgs-to-gif = pkgs.callPackage "${import ./nixos/clansty-flake.nix}/packages/tgs-to-gif" { };
+          prisma-patched = pkgs.callPackage ./nixos/prismaPatched.nix { };
+          fontsSf = pkgs.callPackage ./nixos/fontsSf.nix { };
+          docker = import ./docker.nix {
+            inherit pkgs flakePkgs;
+            nix2container = nix2containerPkgs.nix2container;
           };
-          devShells.default = import ./shell.nix { inherit pkgs flakePkgs; };
-        }
-      );
+        };
+        devShells.default = import ./shell.nix { inherit pkgs flakePkgs; };
+      }
+    );
 }
